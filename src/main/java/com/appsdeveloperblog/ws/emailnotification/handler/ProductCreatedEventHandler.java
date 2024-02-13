035fa5524bd0098c0bfe445e9c3c801ffc1ac965
Jerry Lumontod <>
2024-02-13 16:35:40 +0800

    example on how to use Bearer <token> on resttemplate

--- src/main/java/com/appsdeveloperblog/ws/emailnotification/handler/ProductCreatedEventHandler.java
+++ src/main/java/com/appsdeveloperblog/ws/emailnotification/handler/ProductCreatedEventHandler.java
@@ -2,9 +2,7 @@ package com.appsdeveloperblog.ws.emailnotification.handler;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -15,6 +13,9 @@ import org.springframework.web.client.RestTemplate;
 import com.appsdeveloperblog.ws.core.ProductCreatedEvent;
 import com.appsdeveloperblog.ws.emailnotification.error.NotRetryableException;
 import com.appsdeveloperblog.ws.emailnotification.error.RetryableException;
+
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.LinkedMultiValueMap;
 
 @Component
 @KafkaListener(topics = "product-created-events-topic")
@@ -36,11 +37,17 @@ public class ProductCreatedEventHandler {
 		LOGGER.info("Received a new event: " + productCreatedEvent.getTitle() + " with productId: "
 				+ productCreatedEvent.getProductId());
 
-/*		String requestUrl = "http://localhost:8082/response/200";
+		String requestUrl = "http://localhost:8082/response/200";
 
 		try {
-			ResponseEntity<String> response = restTemplate.exchange(requestUrl, HttpMethod.GET, null, String.class);
 
+			HttpHeaders headers = new HttpHeaders();
+			headers.set("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ5OTEyM0liRFRoYllfQ1hONjF2UEhUeVloWE5XOWZULTZUSzNtemp5Z0JNIn0.eyJleHAiOjE3MDc4MDY2NDAsImlhdCI6MTcwNzgwNjM0MCwianRpIjoiYjAzOTA3NmItNGRiNS00ZDBmLWEyMTAtOWQ5ZmFjNDlhZjZiIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDAwL3JlYWxtcy9hcHBzZGV2ZWxvcGVyYmxvZyIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiJhMDg5NTgxYS1lZDc4LTQxYTktOGIyOS0wMmU2OWUyMzc5YzEiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJwaG90by1hcHAtY29kZS1mbG93LWNsaWVudCIsInNlc3Npb25fc3RhdGUiOiJlMDVhNzBkOC05OWIxLTQ5ZjktYjUwZC04YjQ5YmJiNzFlOTIiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbIiJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsiZGVmYXVsdC1yb2xlcy1hcHBzZGV2ZWxvcGVyYmxvZyIsIm9mZmxpbmVfYWNjZXNzIiwiZGV2ZWxvcGVyIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBlbWFpbCBwcm9maWxlIiwic2lkIjoiZTA1YTcwZDgtOTliMS00OWY5LWI1MGQtOGI0OWJiYjcxZTkyIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoiZmlyc3RuYW1lIGxhc3RuYW1lIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiZGV2IiwiZ2l2ZW5fbmFtZSI6ImZpcnN0bmFtZSIsImZhbWlseV9uYW1lIjoibGFzdG5hbWUiLCJlbWFpbCI6ImVAbWFpbC5jb20ifQ.dOEwtN24WTqhPyASJQKI_jn1bFB7bipwwUoN3Ule5ou6uZ9tl5At_3wVtsVlPpLA32WQWGTB_Q7NsqKATYLFNg-dtln4Ax74D7vUHfJA6P2rdoW8nOTCr8_HA0aTNI60hgA_8myyZuS9b4S6uMhTpfs-qgONC03Or9BXvGiIbR4OxXEpYu96UbPslXuyta08HEbfJEU77JTZF0elbqBgEuG9Q_dzSBuzZrAZ-2k3TYshx0tiWwqYNTxe2C7fWSoHBOFSNSguDJow-BYrQFar1SIiMTpl9W_5-oGAhZe8mM4d7lAIM9YLRI4rs9q2UXFqvC1pVR7_QiYPI2rmp7JSzw"); //accessToken can be the secret key you generate.
+			headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+			HttpEntity <String> entity = new HttpEntity <> (headers);
+			ResponseEntity <String> response = restTemplate.exchange(requestUrl, HttpMethod.GET, entity, String.class);
+
+			//ResponseEntity<String> response = restTemplate.exchange(requestUrl, HttpMethod.GET, null, String.class);
 			if (response.getStatusCode().value() == HttpStatus.OK.value()) {
 				LOGGER.info("Received response from a remote service: " + response.getBody());
 			}
@@ -53,7 +60,7 @@ public class ProductCreatedEventHandler {
 		} catch (Exception ex) {
 			LOGGER.error(ex.getMessage());
 			throw new NotRetryableException(ex);
-		}*/
+		}
 
 	}
 
